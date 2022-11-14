2d44df51a87aca955e9c54d7877f0f8bddc684e9
dandi16 <rramadandi16@gmail.com>
2022-11-14 20:13:16 +0700

    add github page link

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,10 @@
   <div class="layout">
     <ToDoList/>
   </div>
+  <a id="github" href="https://github.com/rizkiramadandi/vue-to-do-list" rel="noreferrer noopener">
+    <span>Github</span>
+    <img src="/src/assets/github.svg" alt="Github">
+  </a>
 </template>
 
 <style>
